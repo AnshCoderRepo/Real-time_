f53eeb89244dbd0716eb1c45be3a953665139a73
Ansh Adarsh <anshk1126@gmail.com>
2024-10-23 10:01:20 +0530

    new changes

--- config/config.py
+++ config/config.py
@@ -8,12 +8,12 @@ class Config:
     API_KEY = os.getenv('OPENWEATHERMAP_API_KEY')
     API_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
     UPDATE_INTERVAL = 300  # 5 minutes in seconds
-    CITIES = ['Delhi', 'Mumbai', 'Chennai', 'Bangalore', 'Kolkata', 'Hyderabad']
+    CITIES = ['Delhi', 'Mumbai', 'Chennai', 'Bangalore', 'Kolkata', 'Hyderabad','Patna','Gopalganj']
     TEMPERATURE_UNIT = 'fahrenheit'  # or 'fahrenheit'
     
     # Alerting thresholds
     MAX_TEMPERATURE_THRESHOLD = 35  # in Celsius
-    CONSECUTIVE_ALERTS = 2
+    CONSECUTIVE_ALERTS = 5
 
     # Database configuration (you can modify this based on your database choice)
     DATABASE_URL = 'mongodb+srv://anshk1126:g7NAsQWxj9vpbd8o@cluster0.e2d9k.mongodb.net/'
